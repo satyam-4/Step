2dcf6dec8fee5221787ccf187c3ebe74d7853d5d
Satyam <sm2003satyam@gmail.com>
2025-07-11 07:53:19 +0530

    feat: enhance Header component with theme toggle and improved layout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Header = () => {
+    const [theme, setTheme] = useState("light");
+
     return (
-        <div className="px-10 py-2 flex items-center justify-between">
-            <h1 className="font-extrabold tracking-wider text-3xl font-mono text-primary"> 
-                STEP
+        <nav className="h-[60px] px-5 sm:px-10 py-2 flex items-center justify-between border-2 border-zinc-200 shadow">
+            <h1 className="font-extrabold text-3xl font-mono text-primary"> 
+                S.T.E.P
             </h1>
-            <button className="bg-primary px-4 py-2 text-white rounded-md font-semibold">
-                Sign In
-            </button>
-        </div>
+            <div className="flex items-center gap-5 sm:gap-10">
+                <button className={`${theme === "light" ? 'text-zinc-600' : 'text-zinc-100'} cursor-pointer`}>
+                    {
+                        theme === "light" 
+                        ? <DarkModeIcon />
+                        : <LightModeIcon />
+                    }
+                    
+                </button>
+                <button className="bg-button-bg hover:bg-button-bg-hover px-4 py-2 text-white rounded-md font-semibold cursor-pointer">
+                    Sign In
+                </button>
+            </div>
+        </nav>
     );
 };
 
